3e356df2a67a76b144586eb93d1cd166c52a5ef1
Yu Yin <yxonic@gmail.com>
2022-09-26 17:22:33 +0800

    feat: add git abbrs

--- conf.d/abbrs.fish
+++ conf.d/abbrs.fish
@@ -1,5 +1,11 @@
 # git
 abbr -a -g -- ga git add
+abbr -a -g -- gst git status
+abbr -a -g -- gco git checkout
+abbr -a -g -- gcm git commit
+abbr -a -g -- gcmsg git commit -m
+abbr -a -g -- gp git push
+abbr -a -g -- gl git pull
 
 # misc
 abbr -a -g -- p proxy
